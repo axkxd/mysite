782ecc57be605549e2f973c4057f78b19bf19536
axkxd <altermaks375@gmail.com>
2023-05-15 18:08:16 +0300

    finished chapter 1

---
+++ mysite/blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, re_path
+from . import views
+
+app_name = 'blog'
+urlpatterns = [
+    # post views
+    # path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
+    re_path(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'\
+        r'(?P<post>[-\w]+)/$',
+        views.post_detail,
+        name='post_detail'),
+]
