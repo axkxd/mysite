9315a053b3d44848d5534edd6ec970fc99d5a80a
axkxd <altermaks375@gmail.com>
2023-05-18 20:47:55 +0300

    add_func

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+from taggit.managers import TaggableManager
+
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -17,6 +19,7 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    tags = TaggableManager()
 
     class Meta:
         ordering = ('-publish',)
@@ -32,3 +35,19 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return f"Comment by {self.name} on {self.post}"
--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, re_path
-from . import views
+from .views import PostListView, post_detail, post_share
 
 app_name = 'blog'
 urlpatterns = [
     # post views
     # path('', views.post_list, name='post_list'),
-    path('', views.PostListView.as_view(), name='post_list'),
+    path('', PostListView.as_view(), name='post_list'),
+    path('tag/<slug:tag_slug>/', PostListView.as_view(), name='post_list_by_tag'),
     re_path(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'\
-        r'(?P<post>[-\w]+)/$',
-        views.post_detail,
+        r'(?P<post>[-\w]+)/$', post_detail,
         name='post_detail'),
+
+    path('<post_id>/share/', post_share, name='post_share'),
 ]
